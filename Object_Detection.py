d2ab25939ede5c9de5cf9b09b5c359b1bcf842b2
Vorawut <vorawut.pun@mail.kmutt.ac.th>
2020-11-28 11:06:42 +0700

    Comment OpenCV code

--- Object_Detection.py
+++ Object_Detection.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import time
 
+# Load Yolo
 net = cv2.dnn.readNet("yolov3_training_last.weights", "yolov3_testing.cfg")
 classes = []
 with open("classes.txt", "r") as f:
@@ -10,6 +11,7 @@ layer_names = net.getLayerNames()
 output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 colors = np.random.uniform(0, 255, size=(len(classes), 3))
 
+# Loading image
 cap = cv2.VideoCapture(0)
 
 font = cv2.FONT_HERSHEY_PLAIN
@@ -17,15 +19,17 @@ starting_time = time.time()
 frame_id = 0
 while True:
     _, frame = cap.read()
-    frame_id += 30
+    frame_id += 1
 
     height, width, channels = frame.shape
 
+    # Detecting objects
     blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
 
     net.setInput(blob)
     outs = net.forward(output_layers)
 
+    # Showing informations on the screen
     class_ids = []
     confidences = []
     boxes = []
@@ -35,12 +39,13 @@ while True:
             class_id = np.argmax(scores)
             confidence = scores[class_id]
             if confidence > 0.2:
-
+                # Object detected
                 center_x = int(detection[0] * width)
                 center_y = int(detection[1] * height)
                 w = int(detection[2] * width)
                 h = int(detection[3] * height)
 
+                # Rectangle coordinates
                 x = int(center_x - w / 2)
                 y = int(center_y - h / 2)
 
@@ -59,6 +64,11 @@ while True:
             cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
             cv2.putText(frame, label + " " + str(round(confidence, 2)), (x, y + 30), font, 3, color, 3)
 
+
+
+    elapsed_time = time.time() - starting_time
+    fps = frame_id / elapsed_time
+    cv2.putText(frame, "FPS: " + str(round(fps, 2)), (10, 50), font, 4, (0, 0, 0), 3)
     cv2.imshow("Image", frame)
     key = cv2.waitKey(1)
     if key == 27:
